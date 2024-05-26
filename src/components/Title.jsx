77c2d5af422626d4adce5c79bf6048603a64eb5d
andresH <andreshg9203@gmail.com>
2024-05-26 10:42:43 -0400

    feat: add component Title

---
+++ src/components/Title.jsx
@@ -0,0 +1,11 @@
+const Title = () => {
+  return (
+    <div className="container-fluid text-center my-4 bg-dark">
+      <h1 className="fw-bolder display-4 text-warning">
+        CONTADOR DE SEGUNDOS CON SetInterval() y ReactJS
+      </h1>
+    </div>
+  );
+};
+
+export default Title;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Counter from "./components/Counter";
+import Title from "./components/Title";
+
 
 let count1 = 0;
-
 setInterval(() => {
   ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
+      <Title />
       <Counter seconds={count1++}/>
     </React.StrictMode>
   );
